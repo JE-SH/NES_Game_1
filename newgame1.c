650e6dd3d20ab14a009a72a05194cbfebd870045
Jose Santana <73598536+JE-SH@users.noreply.github.com>
2022-12-05 17:09:32 -0600

    New changes

--- newgame1.c
+++ newgame1.c
@@ -14,32 +14,29 @@
 #include "vrambuf.h"
 //#link "vrambuf.c"
  
-//**
-//Definiciones dentro del código
-#define NUM_ACTORS 24
-#define COLS 30		// floor width in tiles
+//*********************************************************************
+//DEFINITIONS 
+#define NUM_Objects 1
+#define LIVES 3
+//#define COLS 30		
+//#define CH_FLOOR 0xf4
 
-#define CH_FLOOR 0xf4
+//*****INTEGERS*****//
 int Walk=0; //1 si camina, 0 si no
 int LorR=0; // 0 si es izquierda, 1 si es derecha
-
+//*****ACTOR BYTES*****//
 byte actor_x; //coordenada del personaje
 byte actor_dx; //movimiento de personaje
+//*****OTHER VARIABLES*****//
 byte vel=1;
 byte n_random;
 byte vel_changer=6;
-
-int obj1_y=0;
-int obj2_y=0;
-int obj1_x=0;
-int obj2_x=0;
-int obj1_id=0;
-int obj2_id=0;
-
 static byte score = 0;
-static byte bad_score=3;
+static byte bad_score=LIVES+1;
+
+//-----------------------------------------------------------------------------
 ///----- METASPRITES -----///
-// define a 2x2 metasprite
+// 			2x2 metasprite
 #define DEF_METASPRITE_2x2(name,code,pal)\
 const unsigned char name[]={\
         0,      0,      (code)+0,   pal, \
@@ -48,7 +45,7 @@ const unsigned char name[]={\
         8,      8,      (code)+3,   pal, \
         128};
 
-// define a 2x2 metasprite, flipped horizontally
+//			2x2 metasprite, flipped horizontally
 #define DEF_METASPRITE_2x2_FLIP(name,code,pal)\
 const unsigned char name[]={\
         8,      0,      (code)+0,   (pal)|OAM_FLIP_H, \
@@ -57,18 +54,21 @@ const unsigned char name[]={\
         0,      8,      (code)+3,   (pal)|OAM_FLIP_H, \
         128};
 
-// define a 2x1 metasprite width
+// 			2x1 metasprite width
 #define DEF_METASPRITE_2x1_W(name,code,pal)\
 const unsigned char name[]={\
         0,      0,      (code)+0,   pal, \
         8,      0,      (code)+1,   pal, \
         128};
-// define a 2x1 metasprite height
+
+// 			2x1 metasprite height
 #define DEF_METASPRITE_2x1_H(name,code,pal)\
 const unsigned char name[]={\
         0,      0,      (code)+0,   pal, \
         0,      8,      (code)+1,   pal, \
         128};
+
+// 			2x1 metasprite height, flipped 
 #define DEF_METASPRITE_2x1_H_F(name,code,pal)\
 const unsigned char name[]={\
         0,      0,      (code)+0,   (pal)|OAM_FLIP_H, \
@@ -96,6 +96,7 @@ DEF_METASPRITE_2x1_W(basket2,0x8c,0);
 DEF_METASPRITE_2x1_H(cloud1,0x86,2);
 DEF_METASPRITE_2x1_H_F(cloud2,0x86,2);
 
+//Player Walking
 const unsigned char* const playerWalkSeq[16]={
   playerLRun1, playerLRun2, playerLRun3, 
   playerLRun1, playerLRun2, playerLRun3, 
@@ -105,7 +106,7 @@ const unsigned char* const playerWalkSeq[16]={
   playerRRun1, playerRRun2,
 };
 
-
+//NOT USED BY THE MOMENT
 const char ATTRIBUTE_TABLE[0x625] = {
 
 };
@@ -123,68 +124,35 @@ const char PALETTE[32] = {
   0x0D,0x2D,0x3A,0x00,	// sprite palette 2
   0x0D,0x27,0x2A	// sprite palette 3
 };
+//-----------------------------------------------------------------------------^
 //////////////////////////////////////////////////////////////
 //** ESTRUCTURAS (OBJETOS)
-typedef struct Object {
+typedef struct Objects {
   byte x;
   byte y;
   int id;
-  bool isActive;
-}Object;
+  bool notActive; // 0= Activo   1=NO ACTIVO
+}Objects;
 
-/////////////////////////////////////////////////////////////
-bool collision(byte obj)
-{
-  if(obj==1){
-    if(obj1_y>=185 && obj1_y<=190)
-    {
-      //mbox=;
-      if(obj1_x>=actor_x-30 && obj1_x<=actor_x+14)
-      {
-        score = bcd_add(score, 1);
-        --vel_changer;
-        return true;
-      }
-      else       --bad_score;
-    }
-  }
-  else if(obj==2)
-  {
-     if(obj2_y>=185&&obj2_y<=190)
-     {
-       if(obj2_x>=actor_x-30 && obj2_x<=actor_x+14)
-      {
-         score = bcd_add(score, 1);
-        --vel_changer;
-         return true;
-      }
-       else       --bad_score;
-     }
-  }
-  return false;
-}
+// ARRAY OF OBJECTS
+Objects object[NUM_Objects];
 
-void changeVelocity()
-{
-  if(vel_changer<=0)
-  {
-    ++vel;
-    vel_changer=6;
-  }
-}
-
-///////////////////////
+/////////////////////////////////////////////////////////////^
+//____________________________________________________________________________
+// OAM OFF -*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 void draw_scoreboard() {
   byte i,sum;
+  
   oam_off = oam_spr(24+0, 24, '0'+(score >> 4), 2, oam_off);
   oam_off = oam_spr(24+8, 24, '0'+(score & 0xf), 2, oam_off);
-  for(i=0;i<bad_score;++i)
+  
+  for(i=0;i<bad_score-1;++i)
   {
     sum=(i)*(12);
     oam_meta_spr_pal(200+sum, 24, 0, playerLStand);
   }
 }
-
+// ACTOR-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 // *AVANZAR SI SE PRESIONA ALGUN BOTÓN *//
 void moveActor(){
    // move actor[i] left/right
@@ -238,66 +206,86 @@ void drawActor()
 
 }
 
+// OBJECTS -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 // *GENERADOR DE NÚMEROS ALEATORIOS* //
-void randomPlace()
+void randomPlace(byte i)
 {
-  if(obj1_y==0)
+  struct Objects* o = &object[i];
+  if(o->x==0)
   {
-    obj1_x=(rand8()%224);
-  }
-  if(obj2_y==0)
-  {
-    obj2_x=(rand8()%224);
-  }
+    o->x=(rand8()%224);
+  }  
+}
+//
+bool collision(byte obj)
+{
+  struct Objects* o = &object[obj];
+  byte notactive = o-> notActive;
   
+  if(!notactive) //SI ESTÁ ACTIVO
+  {
+    byte oy = o->y;
+    byte ox = o->x;
+  
+      if(oy >= 185)
+      {
+      //mbox=;
+        if(ox >= actor_x-30 && ox <= actor_x+14)
+        {
+          score = bcd_add(score, 1);
+          --vel_changer;
+          o->notActive=1;
+          
+          return true;
+        }
+        else {
+          --bad_score;
+          o->notActive=1;
+        }
+      }
+    return false;
+  }
+}
+
+void changeVelocity()
+{
+  if(vel_changer<=0)
+  {
+    ++vel;
+    vel_changer=6;
+  }
 }
 
 // *DIBUJAR ITEMS* //
-void drawItem() 
+void drawItem(byte ob) 
 {
  // if(obj1
-  if(obj2_y>=100)
-  {
-    obj1_y+=vel;
-    obj2_y+=vel;
+  struct Objects* o = &object[ob];
+  byte oy = o->y;
+  
+  if( oy > 0 && oy < 200){
+    o->y +=vel;
     
-    if(obj2_y >=200)
-    {
-      obj2_y=0;
-    }
-  }
-  else if(obj1_y>=100)
-  {
-    obj2_y+=vel;
-    obj1_y+=vel; 
-    
-    if(obj1_y >=200)
-    {
-      obj1_y=0;
-    }
-  }
-  else {
-    
-   obj1_y+=vel; 
-  }
-  if(obj1_y>0)
-  {
-    if(!collision(1))
-      oam_meta_spr_pal(obj1_x+8, obj1_y,2, playerLJump);
+    if(!collision(ob))
+      oam_meta_spr_pal(o->x +8, o->y ,2, playerLJump);
     else
-      obj1_x=240;
+      o->x=240;
+  
   }
-  if(obj2_y>0)
-  {
-    if(!collision(2))
-      oam_meta_spr_pal(obj2_x+8, obj2_y,2, playerLJump );
-     else
-      obj2_x=240;
+  else{
+    o->y =1;
+    o->x =0;
+    o->notActive=0;
   }
+  
+ // if(ob == 0 && oy ==100 && object[1].y == 0){
+ //   object[1].y = 1;
+ // }
+ 
   delay(1);
   //ppu_wait_frame();
-}
 
+}
 //////////////////////////////////////////////////////////////
 
 // setup PPU and tables
@@ -309,20 +297,40 @@ void setup_graphics() {
   // enable rendering
   ppu_on_all();
 }
+void setup_objects(){
+  
+  byte i;
+  for(i =0;i<NUM_Objects;++i)
+  {
+    object[i].x = 0;
+    object[i].y = 0;
+    object[i].notActive = 0;
+    
+  }
+}
 
 //*****INICIO DEL JUEGO*****//
 void mainGame(){
-    randomPlace(); 	//Elige lugar aleatorio 
-    drawActor(); 	//Dibuja al actor principal
-    moveActor();	//Mueve al actor
-    drawItem();		//Dibuja el objeto
+  byte i;
+  drawActor(); 	//Dibuja al actor principal
+  moveActor();	//Mueve al actor
+  for(i=0;i<NUM_Objects;++i){
+    randomPlace(i); 	//Elige lugar aleatorio para el objeto i
+    drawItem(i);		//Dibuja el objeto
+  }
     changeVelocity();
+}
+void gameover()
+{
+  vram_adr(NTADR_A(2,20)); // Coordenadas/dirección para poner suelo
+  vram_write("GAME OVER",9);
 }
 void main(void)
 {
   
   setup_graphics(); // Preparación de gráficos
-  obj1_x=200;
+  //obj1_x=200;
+  object[0].y=1;
   actor_x=128;	//coordenada del personaje en x al inicio del juego
   actor_dx=0;   //bytes en movimiento del personaje
   vram_adr(NTADR_A(0,27)); // Coordenadas/dirección para poner suelo
@@ -333,12 +341,21 @@ void main(void)
   while(1) {
 
     //--- *** ---//
+    if(bad_score>0)
+    {
+    draw_scoreboard();	//Dibuja puntaje
+      
     mainGame();
-   
+      
+    }
+    else{ //GAMEOVER
+      break;
+    }
     oam_clear();	//Limpia oam
     
-    draw_scoreboard();	//Dibuja puntaje
     //collision();
     
   }
+      gameover();
+  
 }
